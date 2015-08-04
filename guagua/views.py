4b95e07a43c18ae4b975554c31c9e3b9c4d46ea4
william <csak4718@gmail.com>
2015-08-04 18:52:47 +0800

    Now can get data from comments_list; Comment out redundant, ex: msg list

--- guagua/views.py
+++ guagua/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-import json,httplib
+import json,httplib,urllib
 from django.template import RequestContext, loader
 from .models import Question
 
@@ -16,46 +16,103 @@ def index(request):
        "X-Parse-Application-Id": "iMyUdfPQnXeU1bTHi3f8jhRw5oCx40UxvMfcicno",
        "X-Parse-REST-API-Key": "9CuCkIj1wCODNiCpj9lOT8LfvOTKduf5fJeQa9lc"
      })
-    r = json.loads(connection.getresponse().read())
-    context = {'question': r['results']}
-    print r['results'][0]
+    prayer = json.loads(connection.getresponse().read())
+    context = {'question': prayer['results']}
+    print prayer['results'][0]
     return HttpResponse("Hello, world. You're at the guagua index.")
 
-idx=0
+prayer_idx=0
+# comment_idx=0
+
 def results(request, question_id):
     #latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    global idx
+    global prayer_idx
+    comments_list = [] # comments dictionary of a post
+    # messages=[]
+    # global comment_idx
     connection = httplib.HTTPSConnection('api.parse.com', 443)
     connection.connect()
     connection.request('GET', '/1/classes/Prayer', '', {
        "X-Parse-Application-Id": "iMyUdfPQnXeU1bTHi3f8jhRw5oCx40UxvMfcicno",
        "X-Parse-REST-API-Key": "9CuCkIj1wCODNiCpj9lOT8LfvOTKduf5fJeQa9lc"
      })
-    r = json.loads(connection.getresponse().read())
+    prayer = json.loads(connection.getresponse().read())
 
-    for i in range(0, len(r['results'])):
-        if (r['results'][i]['objectId']==question_id):
-            idx=i
+    connection.request('GET', '/1/classes/Comments', '', {
+       "X-Parse-Application-Id": "iMyUdfPQnXeU1bTHi3f8jhRw5oCx40UxvMfcicno",
+       "X-Parse-REST-API-Key": "9CuCkIj1wCODNiCpj9lOT8LfvOTKduf5fJeQa9lc"
+    })
+    comments = json.loads(connection.getresponse().read())
 
-    if r['results'][idx]['A'] == 0 and r['results'][idx]['B'] == 0:
+
+
+    # params = urllib.urlencode({"include":"comments"})
+    # connection.request('GET', '/1/classes/Prayer/'+question_id+'?%s' % params, '', {
+    #    "X-Parse-Application-Id": "iMyUdfPQnXeU1bTHi3f8jhRw5oCx40UxvMfcicno",
+    #    "X-Parse-REST-API-Key": "9CuCkIj1wCODNiCpj9lOT8LfvOTKduf5fJeQa9lc"
+    # })
+
+
+    # params = urllib.urlencode({"where":json.dumps({
+    #    "PostId": {
+    #      "__type": "Pointer",
+    #      "className": "Prayer",
+    #      "objectId": "uMNQvwpBr5"
+    #    }
+    # })})
+    # connection.request('GET', '/1/classes/Comments?%s' % params, '', {
+    #    "X-Parse-Application-Id": "iMyUdfPQnXeU1bTHi3f8jhRw5oCx40UxvMfcicno",
+    #    "X-Parse-REST-API-Key": "9CuCkIj1wCODNiCpj9lOT8LfvOTKduf5fJeQa9lc"
+    #  })
+
+    # comments = json.loads(connection.getresponse().read())
+
+
+    for i in range(0, len(prayer['results'])):
+        if (prayer['results'][i]['objectId']==question_id):
+            prayer_idx=i
+
+    # Get the comments dictionary of a post
+    for i in range(0, len(comments['results'])):
+        if (comments['results'][i]['PostId']==question_id):
+            comments_list.append(comments['results'][i])
+
+    if prayer['results'][prayer_idx]['A'] == 0 and prayer['results'][prayer_idx]['B'] == 0:
         percentA = 0
         percentB = 0
     else:
-        percentA = 100* r['results'][idx]['A'] / (r['results'][idx]['A']+r['results'][idx]['B'])
-        percentB = 100* r['results'][idx]['B'] / (r['results'][idx]['A']+r['results'][idx]['B'])
+        percentA = 100* prayer['results'][prayer_idx]['A'] / (prayer['results'][prayer_idx]['A']+prayer['results'][prayer_idx]['B'])
+        percentB = 100* prayer['results'][prayer_idx]['B'] / (prayer['results'][prayer_idx]['A']+prayer['results'][prayer_idx]['B'])
+
+    # # get messages out of comments dictionary
+    # for i in range(0, len(comments_list)):
+    #     messages.append(comments_list[i]['msg'])
 
     context = {
-            'numA': r['results'][idx]['A'],
-            'numB': r['results'][idx]['B'],
+            'numA': prayer['results'][prayer_idx]['A'],
+            'numB': prayer['results'][prayer_idx]['B'],
             'progressA': percentA,
             'progressB': percentB,
-            'choiceA': r['results'][idx]['QA'],
-            'choiceB': r['results'][idx]['QB'],
-            'title': r['results'][idx]['prayer'],
-            # 'tag': r['results'][idx]['tag'],
-            'createdAt': r['results'][idx]['createdAt'],
-            'updatedAt': r['results'][idx]['updatedAt'],
+            'choiceA': prayer['results'][prayer_idx]['QA'],
+            'choiceB': prayer['results'][prayer_idx]['QB'],
+            'title': prayer['results'][prayer_idx]['prayer'],
+            # 'tag': prayer['results'][prayer_idx]['tag'],
+            'createdAt': prayer['results'][prayer_idx]['createdAt'],
+            'updatedAt': prayer['results'][prayer_idx]['updatedAt'],
+
+            'comments_list_size': len(comments_list),
+            'comments_list': comments_list,
+            # 'messages': messages,
             }
+
+
+
+    # for i in range(0, len(comments_list)):
+    #     print 'msg'+str(i)
+    #     context['comments_msg'+str(i)] = comments_list[i]['msg']
+
+
+
     return render(request, 'guagua/results.html', context)
 
 
